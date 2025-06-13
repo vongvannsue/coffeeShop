a0bd50c02f8a9117fd30ac663937b793a11d1880
Sue168 <cambodiavannser@gmail.com>
2025-06-14 06:19:15 +0700

    day3

--- coffeeapp/admin.py
+++ coffeeapp/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from . models import coffee
+from . models import coffee,biography
 
+# coffee
 class CoffeeAdmin(admin.ModelAdmin):
-    list_display = ('name','price','quantity')
+    list_display = ('name','price','quantity','image')
+    search_fields = ('name',)
+
+# biography
+class BiographyAdmin(admin.ModelAdmin):
+    list_display = ('name','gender','email','mobile','role','hobby','place_of_birth','language')
+    search_fields = ('name',)
+
 # Register your models here.
 admin.site.register(coffee,CoffeeAdmin)
+admin.site.register(biography,BiographyAdmin)
--- coffeeapp/models.py
+++ coffeeapp/models.py
@@ -1,8 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+# Coffee models
 class coffee(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     quantity = models.IntegerField()
     image = models.CharField(max_length=2083)
+
+# Biography models
+class biography(models.Model):
+    name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=100)
+    place_of_birth = models.CharField(max_length=255)
+    data_birth = models.DateTimeField()
+    role =models.CharField(max_length=255)
+    email = models.EmailField()
+    mobile = models.IntegerField()
+    hobby = models.CharField(max_length=255)
+    language = models.CharField(max_length=255)
